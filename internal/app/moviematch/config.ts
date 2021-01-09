5bb52a3fa7e38a7187ce75f8d4ce0e193308785c
Luke Channings <luke@channings.me>
2021-01-09 20:57:08 +0000

    Fix for #36

--- internal/app/moviematch/config.ts
+++ internal/app/moviematch/config.ts
@@ -15,34 +15,52 @@ export interface Config {
   basicAuth?: BasicAuth;
   devMode: boolean;
   requirePlexLogin: boolean;
+  useTestFixtures: boolean;
+  writeFixtures: boolean;
 }
 
+let currentConfig: Config;
+
+const getTrimmedEnv = (key: string): string | undefined => {
+  const value = Deno.env.get(key);
+  if (typeof value === "string") {
+    return value.trim();
+  }
+};
+
 export const getConfig = (): Config => {
+  if (currentConfig) {
+    return currentConfig;
+  }
+
   const {
-    PLEX_URL = Deno.env.get("PLEX_URL"),
-    PLEX_TOKEN = Deno.env.get("PLEX_TOKEN"),
-    HOST = Deno.env.get("HOST") ?? "127.0.0.1",
-    PORT = Deno.env.get("PORT") ?? 8000,
-    LOG_LEVEL = Deno.env.get("LOG_LEVEL") ?? "INFO",
-    ROOT_PATH = Deno.env.get("ROOT_PATH") ?? "",
-    AUTH_USER = Deno.env.get("AUTH_USER"),
-    AUTH_PASS = Deno.env.get("AUTH_PASS"),
-    REQUIRE_PLEX_LOGIN = Deno.env.get("REQUIRE_PLEX_LOGIN") ?? "0",
-    DEV_MODE = Deno.env.get("DEV_MODE") ?? "0",
+    PLEX_URL = getTrimmedEnv("PLEX_URL"),
+    PLEX_TOKEN = getTrimmedEnv("PLEX_TOKEN"),
+    HOST = getTrimmedEnv("HOST") ?? "127.0.0.1",
+    PORT = getTrimmedEnv("PORT") ?? 8000,
+    LOG_LEVEL = getTrimmedEnv("LOG_LEVEL") ?? "INFO",
+    ROOT_PATH = getTrimmedEnv("ROOT_PATH") ?? "",
+    AUTH_USER = getTrimmedEnv("AUTH_USER"),
+    AUTH_PASS = getTrimmedEnv("AUTH_PASS"),
+    REQUIRE_PLEX_LOGIN = getTrimmedEnv("REQUIRE_PLEX_LOGIN") ?? "0",
+    DEV_MODE = getTrimmedEnv("DEV_MODE") ?? "0",
+    DEV_USE_TEST_FIXTURES = getTrimmedEnv("DEV_USE_TEST_FIXTURES") ?? "",
+    DEV_WRITE_FIXTURES = getTrimmedEnv("DEV_WRITE_FIXTURES") ?? "",
   } = config();
 
   assert(typeof PLEX_URL === "string", "A PLEX_URL is required");
   assert(typeof PLEX_TOKEN === "string", "A PLEX_TOKEN is required");
   assert(
     isLogLevel(LOG_LEVEL),
-    `LOG_LEVEL must be one of ${Object.keys(LogLevels).join(", ")}`,
+    `LOG_LEVEL must be one of ${Object.keys(LogLevels).join(", ")}`
   );
 
-  const basicAuth = !!AUTH_USER && !!AUTH_PASS
-    ? { user: AUTH_USER, password: AUTH_PASS }
-    : undefined;
+  const basicAuth =
+    !!AUTH_USER && !!AUTH_PASS
+      ? { user: AUTH_USER, password: AUTH_PASS }
+      : undefined;
 
-  return {
+  currentConfig = {
     addr: `${HOST}:${PORT}`,
     plexUrl: new URL(`${PLEX_URL}?X-Plex-Token=${PLEX_TOKEN}`),
     logLevel: LOG_LEVEL,
@@ -50,7 +68,11 @@ export const getConfig = (): Config => {
     basicAuth,
     devMode: DEV_MODE === "1",
     requirePlexLogin: REQUIRE_PLEX_LOGIN === "1",
+    useTestFixtures: DEV_USE_TEST_FIXTURES === "1",
+    writeFixtures: DEV_WRITE_FIXTURES === "1",
   };
+
+  return currentConfig;
 };
 
 function isLogLevel(logLevel: string): logLevel is keyof typeof LogLevels {
