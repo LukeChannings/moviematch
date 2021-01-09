58f14a4e5ac69f617052af15dd38889dd13a69fe
Luke Channings <luke@channings.me>
2021-01-09 21:04:00 +0000

    Replace any instance of x-plex-token=<token> with x-plex-token=****

--- internal/app/moviematch/logger.ts
+++ internal/app/moviematch/logger.ts
@@ -1,9 +1,19 @@
 import * as log from "https://deno.land/std/log/mod.ts";
+import { LogRecord } from "https://deno.land/std@0.81.0/log/logger.ts";
 
 export const setupLogger = async (logLevel: keyof typeof log.LogLevels) => {
+  const defaultHandler = new log.handlers.ConsoleHandler(logLevel);
   await log.setup({
     handlers: {
-      console: new log.handlers.ConsoleHandler(logLevel),
+      console: new log.handlers.ConsoleHandler(logLevel, {
+        formatter: (logRecord: LogRecord) => {
+          return defaultHandler
+            .format(logRecord)
+            .replace(/x-plex-token=([^$|&]+)/i, (param, token) =>
+              param.replace(token, "****")
+            );
+        },
+      }),
     },
 
     loggers: {
