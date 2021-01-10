ea4ff14134c540f998971cd38d95974fe1dc4e4c
Luke Channings <luke@channings.me>
2021-01-10 15:21:31 +0000

    Redact the Plex URL in logs

--- internal/app/moviematch/logger.ts
+++ internal/app/moviematch/logger.ts
@@ -1,17 +1,19 @@
 import * as log from "https://deno.land/std/log/mod.ts";
 import { LogRecord } from "https://deno.land/std@0.81.0/log/logger.ts";
+import { getConfig } from "/internal/app/moviematch/config.ts";
 
 export const setupLogger = async (logLevel: keyof typeof log.LogLevels) => {
   const defaultHandler = new log.handlers.ConsoleHandler(logLevel);
+  const { plexUrl } = getConfig();
+  const plexToken = plexUrl.searchParams.get("X-Plex-Token")!;
   await log.setup({
     handlers: {
       console: new log.handlers.ConsoleHandler(logLevel, {
         formatter: (logRecord: LogRecord) => {
           return defaultHandler
             .format(logRecord)
-            .replace(/x-plex-token=([^$"&]+)/i, (param, token) =>
-              param.replace(token, "****")
-            );
+            .replace(plexToken, "****")
+            .replace(plexUrl.hostname, "****");
         },
       }),
     },
