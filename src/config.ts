b51a5f61c43fcb9326510734ed11995d4d436c3c
Luke Channings <luke@channings.me>
2021-02-23 14:43:12 +0000

    Trim trailing '/' in a Plex URL (Fixes #52)

--- src/config.ts
+++ src/config.ts
@@ -1,27 +1,28 @@
 import { config } from 'https://deno.land/x/dotenv@v1.0.1/mod.ts'
 import * as log from 'https://deno.land/std@0.79.0/log/mod.ts'
 
+const conf = config()
+
 const getEnvTrimmed = (name: string) => {
   const value = Deno.env.get(name)
   if (typeof value === 'string') {
     return value.trim()
   }
-  return undefined
+
+  return conf[name]
 }
 
-export const {
-  PLEX_URL = getEnvTrimmed('PLEX_URL'),
-  PLEX_TOKEN = getEnvTrimmed('PLEX_TOKEN'),
-  PORT = getEnvTrimmed('PORT') ?? '8000',
-  LOG_LEVEL = getEnvTrimmed('LOG_LEVEL') ?? 'INFO',
-  MOVIE_BATCH_SIZE = getEnvTrimmed('MOVIE_BATCH_SIZE') ?? '25',
-  LINK_TYPE = getEnvTrimmed('LINK_TYPE') ?? 'app',
-  DEFAULT_SECTION_TYPE_FILTER = getEnvTrimmed('DEFAULT_SECTION_TYPE_FILTER') ??
-    'movie',
-  LIBRARY_FILTER = getEnvTrimmed('LIBRARY_FILTER') ?? '',
-  COLLECTION_FILTER = getEnvTrimmed('COLLECTION_FILTER') ?? '',
-  ROOT_PATH = getEnvTrimmed('ROOT_PATH') ?? '',
-} = config()
+export const PLEX_URL = getEnvTrimmed('PLEX_URL')?.replace(/\/$/, '')
+export const PLEX_TOKEN = getEnvTrimmed('PLEX_TOKEN')
+export const PORT = getEnvTrimmed('PORT') ?? '8000'
+export const LOG_LEVEL = getEnvTrimmed('LOG_LEVEL') ?? 'INFO'
+export const MOVIE_BATCH_SIZE = getEnvTrimmed('MOVIE_BATCH_SIZE') ?? '25'
+export const LINK_TYPE = getEnvTrimmed('LINK_TYPE') ?? 'app'
+export const DEFAULT_SECTION_TYPE_FILTER =
+  getEnvTrimmed('DEFAULT_SECTION_TYPE_FILTER') ?? 'movie'
+export const LIBRARY_FILTER = getEnvTrimmed('LIBRARY_FILTER') ?? ''
+export const COLLECTION_FILTER = getEnvTrimmed('COLLECTION_FILTER') ?? ''
+export const ROOT_PATH = getEnvTrimmed('ROOT_PATH') ?? ''
 
 export const getVersion = async () => {
   const pkgText = await Deno.readTextFile(Deno.cwd() + '/package.json')
