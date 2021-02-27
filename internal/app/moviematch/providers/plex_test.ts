fe87fc28d9cd8bbfda88e50d6bd0dee4916bccac
Luke Channings <luke@channings.me>
2021-02-27 23:14:31 +0000

    Update Plex API test

--- internal/app/moviematch/providers/plex_test.ts
+++ internal/app/moviematch/providers/plex_test.ts
@@ -5,7 +5,7 @@ const TEST_PLEX_URL = Deno.env.get("TEST_PLEX_URL");
 const TEST_PLEX_TOKEN = Deno.env.get("TEST_PLEX_TOKEN");
 
 Deno.test("providers -> plex -> getFilters", async () => {
-  const provider = createProvider({
+  const provider = createProvider("0", {
     url: TEST_PLEX_URL!,
     token: TEST_PLEX_TOKEN!,
   });
