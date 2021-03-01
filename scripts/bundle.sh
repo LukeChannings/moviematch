63e3faa264dafc40db6a97e83e96026cc2287fb7
Luke Channings <luke@channings.me>
2021-03-01 21:00:35 +0000

    Update Dockerfile

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -2,6 +2,14 @@
 
 set -eo pipefail
 
+while getopts e option
+do
+case "${option}"
+in
+e) EXIT_EARLY=1;;
+esac
+done
+
 esbuild web/app/src/main.tsx --bundle --minify --outdir=web/app/dist --format=esm --sourcemap=external --target=es2019
 deno run --unstable -A ./cmd/moviematch/pkger.ts web/static web/template web/app/dist configs/localization VERSION
 
@@ -11,6 +19,10 @@ mkdir -p build
 cat configs/import_map.json | sed 's/pkger.ts/pkger_release.ts/' > build/import_map.json
 
 deno bundle --unstable --import-map=build/import_map.json ./cmd/moviematch/main.ts > build/moviematch.js
+
+if [[ "$EXIT_EARLY" == "1" ]]; then
+  exit 0
+fi
 
 TARGETS=("x86_64-unknown-linux-gnu" "x86_64-pc-windows-msvc" "x86_64-apple-darwin" "aarch64-apple-darwin")
 
