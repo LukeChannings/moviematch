e71edd2477d4b3a4e2e65aa3ab4e56f94bb2d0be
Luke Channings <luke@channings.me>
2021-03-25 09:48:01 +0000

    Don't fail entire test suite if TEST_PLEX_URL and TEST_PLEX_TOKEN are missing (fixes failing tests on PRs). Fail getAllFilters if there are no filters

--- internal/app/moviematch/providers/plex_test.ts
+++ internal/app/moviematch/providers/plex_test.ts
@@ -1,17 +1,30 @@
 import { assert } from "util/assert.ts";
 import { createProvider } from "/internal/app/moviematch/providers/plex.ts";
 
-const TEST_PLEX_URL = Deno.env.get("TEST_PLEX_URL");
-const TEST_PLEX_TOKEN = Deno.env.get("TEST_PLEX_TOKEN");
+try {
+  const TEST_PLEX_URL = Deno.env.get("TEST_PLEX_URL");
+  const TEST_PLEX_TOKEN = Deno.env.get("TEST_PLEX_TOKEN");
 
-Deno.test("providers -> plex -> getFilters", async () => {
-  const provider = createProvider("0", {
-    url: TEST_PLEX_URL!,
-    token: TEST_PLEX_TOKEN!,
-    libraryTypeFilter: ["movie"],
+  assert(
+    typeof TEST_PLEX_URL !== "undefined",
+    "TEST_PLEX_URL is required for testing Plex API integration",
+  );
+  assert(
+    typeof TEST_PLEX_TOKEN !== "undefined",
+    "TEST_PLEX_TOKEN is required for testing Plex API integration",
+  );
+
+  Deno.test("providers -> plex -> getFilters", async () => {
+    const provider = createProvider("0", {
+      url: TEST_PLEX_URL!,
+      token: TEST_PLEX_TOKEN!,
+      libraryTypeFilter: ["movie"],
+    });
+
+    const filters = await provider.getFilters();
+
+    assert(!!filters);
   });
-
-  const filters = await provider.getFilters();
-
-  assert(!!filters);
-});
+} catch (err) {
+  console.error(err);
+}
--- internal/app/plex/api_test.ts
+++ internal/app/plex/api_test.ts
@@ -11,177 +11,184 @@ import { PlexApi } from "/internal/app/plex/api.ts";
 const TEST_PLEX_URL = Deno.env.get("TEST_PLEX_URL");
 const TEST_PLEX_TOKEN = Deno.env.get("TEST_PLEX_TOKEN");
 
-assert(
-  typeof TEST_PLEX_URL !== "undefined",
-  "TEST_PLEX_URL is required for testing Plex API integration",
-);
-assert(
-  typeof TEST_PLEX_TOKEN !== "undefined",
-  "TEST_PLEX_TOKEN is required for testing Plex API integration",
-);
-
-const plexApi = new PlexApi(
-  TEST_PLEX_URL,
-  TEST_PLEX_TOKEN,
-  {
-    language: "en",
-  },
-);
-
-Deno.test("plexApi -> isAvaliable", async () => {
-  assertEquals(
-    await plexApi.isAvaliable(),
-    true,
-    "The server should be available",
+try {
+  assert(
+    typeof TEST_PLEX_URL !== "undefined",
+    "TEST_PLEX_URL is required for testing Plex API integration",
   );
-});
-
-Deno.test("plexApi -> getIdentity", async () => {
-  const identitySchema = object().shape({
-    size: number().required(),
-    claimed: boolean().required(),
-    machineIdentifier: string().required(),
-    version: string().required(),
-  });
-
-  const identity = await plexApi.getIdentity();
-  await identitySchema.validate(identity);
-});
-
-Deno.test("plexApi -> getCapabilities", async () => {
-  const capabilities = await plexApi.getCapabilities();
-
-  assertNotEquals(
-    capabilities.size,
-    0,
-    "The server is expected to have capabilities.",
+  assert(
+    typeof TEST_PLEX_TOKEN !== "undefined",
+    "TEST_PLEX_TOKEN is required for testing Plex API integration",
   );
-});
 
-Deno.test("plexApi -> getServerVersion", async () => {
-  const serverVersionSchema = object().shape({
-    fullVersion: string().required(),
-    major: number().required(),
-    minor: number().required(),
-    patch: number().required(),
-    build: number().required(),
-    meta: string().required(),
-    hash: string().required(),
+  const plexApi = new PlexApi(
+    TEST_PLEX_URL,
+    TEST_PLEX_TOKEN,
+    {
+      language: "en",
+    },
+  );
+
+  Deno.test("plexApi -> isAvaliable", async () => {
+    assertEquals(
+      await plexApi.isAvaliable(),
+      true,
+      "The server should be available",
+    );
   });
 
-  const serverVersion = await plexApi.getServerVersion();
-  await serverVersionSchema.validate(serverVersion);
-});
+  Deno.test("plexApi -> getIdentity", async () => {
+    const identitySchema = object().shape({
+      size: number().required(),
+      claimed: boolean().required(),
+      machineIdentifier: string().required(),
+      version: string().required(),
+    });
 
-Deno.test("plexApi -> getServerName", async () => {
-  assertEquals(typeof await plexApi.getServerName(), "string");
-});
-
-Deno.test("plexApi -> getServerId", async () => {
-  assertEquals(typeof await plexApi.getServerId(), "string");
-});
-
-Deno.test("plexApi -> getServerOwner", async () => {
-  assertEquals(typeof await plexApi.getServerOwner(), "string");
-});
-
-Deno.test("plexApi -> getLibraries", async () => {
-  const librarySchema = object().shape({
-    allowSync: boolean().required(),
-    art: string().required(),
-    composite: string().required(),
-    filters: boolean().required(),
-    refreshing: boolean().required(),
-    thumb: string().required(),
-    key: string().required(),
-    type: string().matches(/^(movie|show|artist|photo)$/).required(),
-    title: string().required(),
-    agent: string().required(),
-    scanner: string().required(),
-    language: string().required(),
-    uuid: string().required(),
-    updatedAt: number().required(),
-    createdAt: number().required(),
-    scannedAt: number().required(),
-    content: boolean().required(),
-    directory: boolean().required(),
-    contentChangedAt: number().required(),
-    hidden: number().required(),
-    Location: array(
-      object().shape({
-        id: number().required(),
-        path: string().required(),
-      }).required(),
-    ).required(),
+    const identity = await plexApi.getIdentity();
+    await identitySchema.validate(identity);
   });
 
-  const libraries = await plexApi.getLibraries();
+  Deno.test("plexApi -> getCapabilities", async () => {
+    const capabilities = await plexApi.getCapabilities();
 
-  for (const library of libraries) {
-    await librarySchema.validate(library);
-  }
-});
-
-Deno.test("plexApi -> getAllFilters", async () => {
-  const filters = await plexApi.getAllFilters();
-
-  const filtersSchema = object().shape({
-    Type: array(
-      object().shape({
-        key: string().required(),
-        type: string().required(),
-        title: string().required(),
-        active: boolean().required(),
-        Filter: array(
-          object().shape({
-            filter: string().required(),
-            filterType: string().required(),
-            key: string().required(),
-            title: string().required(),
-            type: string().required(),
-          }).required(),
-        ),
-      }),
-    ),
-    FieldType: array(
-      object().shape({
-        type: string().required(),
-        Operator: array(
-          object().shape({
-            key: string().required(),
-            title: string().required(),
-          }),
-        ),
-      }),
-    ),
+    assertNotEquals(
+      capabilities.size,
+      0,
+      "The server is expected to have capabilities.",
+    );
   });
 
-  await filtersSchema.validate(filters);
-});
+  Deno.test("plexApi -> getServerVersion", async () => {
+    const serverVersionSchema = object().shape({
+      fullVersion: string().required(),
+      major: number().required(),
+      minor: number().required(),
+      patch: number().required(),
+      build: number().required(),
+      meta: string().required(),
+      hash: string().required(),
+    });
 
-Deno.test("plexApi -> getFilterValues", async () => {
-  const filterValues = await plexApi.getFilterValues("3/collection");
-
-  const filterValueSchema = object().shape({
-    size: number().required(),
-    allowSync: boolean().required(),
-    art: string().required(),
-    content: string().required(),
-    identifier: string().required(),
-    mediaTagPrefix: string().required(),
-    mediaTagVersion: number().required(),
-    thumb: string().required(),
-    title1: string().required(),
-    title2: string().required(),
-    viewGroup: string().required(),
-    viewMode: number().required(),
-    Directory: array(
-      object().shape({
-        key: string().required(),
-        title: string().required(),
-      }),
-    ).required(),
+    const serverVersion = await plexApi.getServerVersion();
+    await serverVersionSchema.validate(serverVersion);
   });
 
-  await filterValueSchema.validate(filterValues);
-});
+  Deno.test("plexApi -> getServerName", async () => {
+    assertEquals(typeof await plexApi.getServerName(), "string");
+  });
+
+  Deno.test("plexApi -> getServerId", async () => {
+    assertEquals(typeof await plexApi.getServerId(), "string");
+  });
+
+  Deno.test("plexApi -> getServerOwner", async () => {
+    assertEquals(typeof await plexApi.getServerOwner(), "string");
+  });
+
+  Deno.test("plexApi -> getLibraries", async () => {
+    const librarySchema = object().shape({
+      allowSync: boolean().required(),
+      art: string().required(),
+      composite: string().required(),
+      filters: boolean().required(),
+      refreshing: boolean().required(),
+      thumb: string().required(),
+      key: string().required(),
+      type: string().matches(/^(movie|show|artist|photo)$/).required(),
+      title: string().required(),
+      agent: string().required(),
+      scanner: string().required(),
+      language: string().required(),
+      uuid: string().required(),
+      updatedAt: number().required(),
+      createdAt: number().required(),
+      scannedAt: number().required(),
+      content: boolean().required(),
+      directory: boolean().required(),
+      contentChangedAt: number().required(),
+      hidden: number().required(),
+      Location: array(
+        object().shape({
+          id: number().required(),
+          path: string().required(),
+        }).required(),
+      ).required(),
+    });
+
+    const libraries = await plexApi.getLibraries();
+
+    for (const library of libraries) {
+      await librarySchema.validate(library);
+    }
+  });
+
+  Deno.test("plexApi -> getAllFilters", async () => {
+    const filters = await plexApi.getAllFilters();
+
+    const filtersSchema = object().shape({
+      Type: array(
+        object().shape({
+          key: string().required(),
+          type: string().required(),
+          title: string().required(),
+          active: boolean().required(),
+          Filter: array(
+            object().shape({
+              filter: string().required(),
+              filterType: string().required(),
+              key: string().required(),
+              title: string().required(),
+              type: string().required(),
+            }).required(),
+          ),
+        }),
+      ),
+      FieldType: array(
+        object().shape({
+          type: string().required(),
+          Operator: array(
+            object().shape({
+              key: string().required(),
+              title: string().required(),
+            }),
+          ),
+        }),
+      ),
+    });
+
+    assertNotEquals(filters.Type.length, 0, "Filters cannot be empty");
+    assertNotEquals(filters.FieldType.length, 0, "Filters cannot be empty");
+
+    await filtersSchema.validate(filters);
+  });
+
+  Deno.test("plexApi -> getFilterValues", async () => {
+    const filterValues = await plexApi.getFilterValues("3/collection");
+
+    const filterValueSchema = object().shape({
+      size: number().required(),
+      allowSync: boolean().required(),
+      art: string().required(),
+      content: string().required(),
+      identifier: string().required(),
+      mediaTagPrefix: string().required(),
+      mediaTagVersion: number().required(),
+      thumb: string().required(),
+      title1: string().required(),
+      title2: string().required(),
+      viewGroup: string().required(),
+      viewMode: number().required(),
+      Directory: array(
+        object().shape({
+          key: string().required(),
+          title: string().required(),
+        }),
+      ).required(),
+    });
+
+    await filterValueSchema.validate(filterValues);
+  });
+} catch (err) {
+  console.log(err);
+}
