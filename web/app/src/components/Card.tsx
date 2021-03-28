43324932878f45fabddbb202949fa629c00fc305
Luke Channings <luke@channings.me>
2021-03-28 21:12:23 +0100

    Show movie details (#9)

--- web/app/src/components/Card.tsx
+++ web/app/src/components/Card.tsx
@@ -1,14 +1,10 @@
-import React, {
-  forwardRef,
-  ReactNode,
-  useState,
-} from "https://cdn.skypack.dev/react@17.0.1?dts";
-import { Media } from "../../../../types/moviematch.ts";
+import React, { forwardRef, ReactNode, useState } from "react";
+import type { Media } from "../../../../types/moviematch";
 
 import "./Card.css";
-import { InfoIcon } from "./InfoIcon.tsx";
-import { Pill } from "./Pill.tsx";
-import { ContentRatingSymbol } from "./ContentRatingSymbol.tsx";
+import { InfoIcon } from "./InfoIcon";
+import { Pill } from "./Pill";
+import { ContentRatingSymbol } from "./ContentRatingSymbol";
 
 export interface CardProps {
   title?: ReactNode;
@@ -19,19 +15,17 @@ export interface CardProps {
 }
 
 const formatTime = (milliseconds: number) =>
-  `${Math.round((milliseconds / 1000) / 60)} minutes`;
-
-const formatList = (list: string[]) => list.join("&");
+  `${Math.round(milliseconds / 1000 / 60)} minutes`;
 
 export const Card = forwardRef<HTMLDivElement & HTMLAnchorElement, CardProps>(
   ({ media, title, href, style }, ref) => {
     const [showMoreInfo, setShowMoreInfo] = useState<boolean>(false);
 
     const srcSet = [
-      `${media.posterUrl}?width=300`,
-      `${media.posterUrl}?width=450 1.5x`,
-      `${media.posterUrl}?width=600 2x`,
-      `${media.posterUrl}?width=900 3x`,
+      `${import.meta.env.API_URI ?? ""}${media.posterUrl}?width=300`,
+      `${import.meta.env.API_URI ?? ""}${media.posterUrl}?width=450 1.5x`,
+      `${import.meta.env.API_URI ?? ""}${media.posterUrl}?width=600 2x`,
+      `${import.meta.env.API_URI ?? ""}${media.posterUrl}?width=900 3x`,
     ];
 
     const mediaTitle = `${media.title}${
@@ -46,11 +40,11 @@ export const Card = forwardRef<HTMLDivElement & HTMLAnchorElement, CardProps>(
         className={`Card ${href ? "--link" : ""}`}
         {...(href
           ? {
-            href,
-            target: /(iPhone|iPad)/.test(navigator.userAgent)
-              ? "_self"
-              : "_blank",
-          }
+              href,
+              target: /(iPhone|iPad)/.test(navigator.userAgent)
+                ? "_self"
+                : "_blank",
+            }
           : {})}
       >
         <img
@@ -59,35 +53,35 @@ export const Card = forwardRef<HTMLDivElement & HTMLAnchorElement, CardProps>(
           srcSet={srcSet.join(", ")}
           alt={`${media.title} poster`}
         />
-        {showMoreInfo
-          ? (
-            <div className="Card_MoreInfo">
-              <div className="Card_MoreInfo_Header">
-                <p className="Card_MoreInfo_Title">{mediaTitle}</p>
-                <div className="Card_MoreInfo_Metadata">
-                  <Pill>{media.year}</Pill>
-                  <Pill>{formatTime(+media.duration)}</Pill>
-                  <Pill>{media.rating} ⭐️</Pill>
-                  <Pill>
-                    <ContentRatingSymbol
-                      rating={media.contentRating!}
-                      size="1.5em"
-                    />
-                  </Pill>
-                  {media.genres.map((genre) => <Pill>{genre}</Pill>)}
-                </div>
+        {showMoreInfo ? (
+          <div className="Card_MoreInfo">
+            <div className="Card_MoreInfo_Header">
+              <p className="Card_MoreInfo_Title">{mediaTitle}</p>
+              <div className="Card_MoreInfo_Metadata">
+                <Pill>{media.year}</Pill>
+                <Pill>{formatTime(+media.duration)}</Pill>
+                <Pill>{media.rating} ⭐️</Pill>
+                <Pill>
+                  <ContentRatingSymbol
+                    rating={media.contentRating!}
+                    size="1.5em"
+                  />
+                </Pill>
+                {media.genres.map((genre) => (
+                  <Pill>{genre}</Pill>
+                ))}
               </div>
-              <p className="Card_MoreInfo_Description">
-                {media.description}
-                {media.description}
-              </p>
             </div>
-          )
-          : (
-            <div className="Card_Info">
-              <p className="Card_Info_Title">{title ?? mediaTitle}</p>
-            </div>
-          )}
+            <p className="Card_MoreInfo_Description">
+              {media.description}
+              {media.description}
+            </p>
+          </div>
+        ) : (
+          <div className="Card_Info">
+            <p className="Card_Info_Title">{title ?? mediaTitle}</p>
+          </div>
+        )}
         <button
           className="Card_MoreInfoButton"
           onClick={(e) => {
@@ -101,5 +95,3 @@ export const Card = forwardRef<HTMLDivElement & HTMLAnchorElement, CardProps>(
     );
   },
 );
-
-Card.displayName = "Card";
