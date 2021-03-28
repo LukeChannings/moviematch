7a8bf70ff8fdfc458a459cb10a643f1be897ad1c
Luke Channings <luke@channings.me>
2021-03-28 21:11:16 +0100

    Reimplement CardStack component (#58)

--- web/app/src/components/CardStack.tsx
+++ web/app/src/components/CardStack.tsx
@@ -1,245 +1,284 @@
 import React, {
-  Children,
-  cloneElement,
-  isValidElement,
-  NamedExoticComponent,
+  memo,
   ReactNode,
   useEffect,
+  useReducer,
   useRef,
   useState,
-} from "https://cdn.skypack.dev/react@17.0.1?dts";
-import { Rate } from "../../../../types/moviematch.ts";
-import { useAnimationFrame } from "../hooks/useAnimationFrame.ts";
-
+} from "react";
+import type { Media } from "../../../../types/moviematch";
 import "./CardStack.css";
-import { Tr } from "./Tr.tsx";
+import { useGesture } from "react-use-gesture";
+import {
+  animated,
+  Controller,
+  ControllerUpdate,
+  Spring,
+} from "@react-spring/web";
+import { Tr } from "./Tr";
+const { abs, sign } = Math;
+
+type Card = Media;
 
 interface CardStackProps {
-  children: ReactNode;
-  onRate(rating: Rate["rating"]): void;
+  cards: Card[];
+  renderCard: (card: Card) => ReactNode;
+  onCardDismissed: (card: Card, direction: "left" | "right") => void;
 }
 
-const createAnimation = (
-  element: HTMLElement,
-  index: number,
-  x = "0",
-  opacity = "1",
-) => {
-  const effect = new KeyframeEffect(
-    element,
-    [
-      {
-        transform: `translate3d( 0, calc(var(--y) * ${index +
-          1}), calc(var(--z) * ${index + 1}) ) rotateX(var(--rotX))`,
-        opacity: "1",
-      },
-      {
-        transform:
-          `translate3d( ${x}, calc(var(--y) * ${index}), calc(var(--z) * ${index}) ) rotateX(var(--rotX))`,
-        opacity,
-      },
-    ],
-    {
-      duration: 1_000,
-      fill: "both",
-      composite: "replace",
-      iterationComposite: "replace",
-    },
-  );
-  const animation = new Animation(effect, document.timeline);
-  animation.pause();
-  return animation;
+type Spring = ControllerUpdate<{
+  x: number;
+  y: number;
+  z: number;
+  opacity: number;
+}>;
+
+const INITIAL_COUNT = 7;
+const YZ_SIZE = 15;
+const YZ_OFFSET = -30;
+
+interface StackItem<T> {
+  id: string;
+  index: number;
+  controller: Controller<Spring>;
+  item: T;
+  removed: boolean;
+}
+
+export const useViewportWidth = (transform?: (n: number) => number) => {
+  const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    const handler = () => setViewportWidth(window.innerWidth);
+    window.addEventListener("resize", handler);
+    return () => window.removeEventListener("resize", handler);
+  }, []);
+  return transform ? transform(viewportWidth) : viewportWidth;
 };
 
-export const CardStack = ({ children, onRate }: CardStackProps) => {
-  const [topCardIndex, setTopCardIndex] = useState<number>(0);
-  const cardStackEl = useRef<HTMLDivElement>(null);
-  const cardEls = useRef(new Map<number, HTMLDivElement>());
-  const cardAnimations = useRef<Animation[] | null>(null);
-  const topCardAnimation = useRef<Animation | null>(null);
-  const animationTime = useRef<number | null>(null);
+export const useFirstChildWidth = (transform?: (n: number) => number) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [width, setWidth] = useState(0);
+  useEffect(() => {
+    const rect = ref.current?.firstElementChild?.getBoundingClientRect();
+    if (rect) {
+      setWidth(transform ? transform(rect.width) : rect.width);
+    }
+  }, [transform, ref]);
+  return [ref, width] as const;
+};
 
-  useAnimationFrame(
-    (() => {
-      let lastTime: number | null = null;
-      return () => {
-        if (
-          animationTime.current !== lastTime &&
-          animationTime.current !== null
-        ) {
-          lastTime = animationTime.current;
-          if (cardAnimations.current !== null) {
-            for (const animation of cardAnimations.current) {
-              animation.currentTime = animationTime.current;
+export const CardStack = memo(
+  ({ cards, renderCard, onCardDismissed }: CardStackProps) => {
+    const vw = useViewportWidth((n) => n / 2);
+    const [elRef, ew] = useFirstChildWidth();
+    const [{ items }, dispatch] = useReducer(
+      function reducer(
+        { items, index }: { items: StackItem<Card>[]; index: number },
+        action:
+          | { type: "add" }
+          | {
+              type: "remove";
+              payload: { id: string; direction: "left" | "right" };
             }
+          | { type: "finalizeRemove"; payload: { id: string } },
+      ) {
+        let newIndex = index;
+        let newItems = items;
+
+        switch (action.type) {
+          case "add": {
+            newIndex = index + 1;
+            if (newIndex > cards.length) {
+              return { items, index };
+            }
+            const [newCard] = cards.slice(index, newIndex);
+            const controller = new Controller<Spring>({
+              x: 0,
+              y: YZ_OFFSET,
+              z: YZ_OFFSET,
+              opacity: 0,
+            });
+            newItems = [
+              {
+                id: newCard.id,
+                item: newCard,
+                index: 0,
+                controller,
+                removed: false,
+              },
+              ...items.map((item) => ({ ...item, index: item.index + 1 })),
+            ];
+
+            controller.start({ opacity: 1 });
+            break;
           }
-          if (topCardAnimation.current !== null) {
-            topCardAnimation.current.currentTime = animationTime.current;
+          case "remove": {
+            const item = items.find((_) => _.id === action.payload.id);
+            if (item) {
+              const itemIndex = items.indexOf(item);
+              item.controller
+                .start({
+                  x: (action.payload.direction === "left" ? -1 : 1) * (vw + ew),
+                })
+                .then(() => {
+                  dispatch({
+                    type: "finalizeRemove",
+                    payload: { id: action.payload.id },
+                  });
+                  onCardDismissed(item.item, action.payload.direction);
+                });
+
+              newItems = items.map((item, i) =>
+                item.id === action.payload.id
+                  ? { ...item, removed: true }
+                  : {
+                      ...item,
+                      index: i > itemIndex ? item.index - 1 : item.index,
+                    },
+              );
+            }
+            break;
+          }
+          case "finalizeRemove": {
+            newItems = newItems.filter((_) => _.id !== action.payload.id);
+            break;
           }
         }
-      };
-    })(),
-  );
 
-  useEffect(
-    function setAnimations() {
-      cardAnimations.current = [...cardEls.current.values()].map((cardEl, i) =>
-        createAnimation(cardEl, i)
-      );
-    },
-    [topCardIndex],
-  );
-
-  useEffect(
-    function handleSwipe() {
-      const handlePointerDown = (startEvent: PointerEvent) => {
-        if (
-          (startEvent.pointerType === "mouse" && startEvent.button !== 0) ||
-          startEvent.target instanceof HTMLButtonElement
-        ) {
-          return;
+        for (const item of newItems) {
+          item.controller.start({
+            y: YZ_SIZE * item.index + YZ_OFFSET,
+            z: YZ_SIZE * item.index + YZ_OFFSET,
+          });
         }
 
-        const topCardEl = cardEls.current.get(0)!;
+        return { index: newIndex, items: newItems };
+      },
+      {
+        items: cards.slice(0, INITIAL_COUNT).map((card, i) => ({
+          id: card.id,
+          index: i,
+          item: card,
+          controller: new Controller<Spring>({
+            x: 0,
+            y: i * YZ_SIZE + YZ_OFFSET,
+            z: i * YZ_SIZE + YZ_OFFSET,
+            opacity: 1,
+          }),
+          removed: false,
+        })),
+        index: INITIAL_COUNT,
+      },
+    );
 
-        startEvent.preventDefault();
-        cardStackEl.current!.setPointerCapture(startEvent.pointerId);
-
-        const animationDuration = 1_000;
-        const maxX = window.innerWidth;
-        let currentDirection: "left" | "right" | null;
-        let position = 0;
-
-        const handleMove = (e: PointerEvent) => {
-          const direction = e.clientX < startEvent.clientX ? "left" : "right";
-          const delta = e.clientX - startEvent.clientX;
-          position = Math.max(
-            0,
-            Math.min(
-              1,
-              direction === "left"
-                ? Math.abs(delta) / startEvent.clientX
-                : delta / (maxX - startEvent.clientX),
-            ),
+    useEffect(() => {
+      const handler = (e: KeyboardEvent) => {
+        if (e.code === "ArrowLeft" || e.code === "ArrowRight") {
+          const item = items.reduceRight<StackItem<Media> | null>(
+            (item, _) => item || (!_.removed ? _ : null),
+            null,
           );
-
-          if (currentDirection != direction) {
-            currentDirection = direction;
-            topCardAnimation.current = createAnimation(
-              topCardEl,
-              0,
-              `${currentDirection === "left" ? "-50vw" : "50vw"}`,
-              "0",
-            );
-          }
-
-          animationTime.current = position * animationDuration;
-        };
-
-        cardStackEl.current!.addEventListener("pointermove", handleMove, {
-          passive: true,
-        });
-
-        cardStackEl.current!.addEventListener(
-          "lostpointercapture",
-          async () => {
-            cardStackEl.current!.removeEventListener("pointermove", handleMove);
-            animationTime.current = null;
-
-            if (!topCardAnimation.current || !cardAnimations.current) {
-              console.error(`No top card animation or cardAnimations`);
-              return;
-            }
-
-            const isCompleteSwipe = position > 0.5;
-
-            if (isCompleteSwipe) {
-              // we don't want the user to get a handle on the card as it's animating out
-              cardStackEl.current!.addEventListener(
-                "pointerdown",
-                handlePointerDown,
-              );
-            }
-
-            try {
-              await Promise.all(
-                [...cardAnimations.current, topCardAnimation.current].map(
-                  async (animation) => {
-                    if (
-                      animation.playState !== "finished" &&
-                      animation.currentTime !== (isCompleteSwipe ? 1_000 : 0)
-                    ) {
-                      // when playbackRate is negative the animation is played in reverse
-                      animation.playbackRate = isCompleteSwipe ? 3 : -3;
-                      animation.play();
-                      await animation.finished;
-                      animation.pause();
-                      animation.playbackRate = 1;
-                    }
-                  },
-                ),
-              );
-            } catch (err) {
-              if (err.name !== "AbortError") {
-                console.error(err);
-              }
-            }
-
-            if (isCompleteSwipe) {
-              onRate(currentDirection === "left" ? "dislike" : "like");
-              setTopCardIndex(topCardIndex + 1);
-            }
-          },
-          { once: true },
-        );
-      };
-
-      const handleTouchStart = (e: Event) => {
-        if (e.target instanceof HTMLButtonElement) {
-          return;
-        }
-        e.preventDefault();
-      };
-
-      cardStackEl.current!.addEventListener("touchstart", handleTouchStart);
-      cardStackEl.current!.addEventListener("pointerdown", handlePointerDown);
-
-      return () => {
-        cardStackEl.current!.removeEventListener(
-          "pointerdown",
-          handlePointerDown,
-        );
-        cardStackEl.current!.removeEventListener(
-          "touchstart",
-          handleTouchStart,
-        );
-      };
-    },
-    [cardStackEl.current, topCardIndex],
-  );
-
-  const isEmpty = Children.count(children) === 0;
-
-  return (
-    <div className={`CardStack ${isEmpty ? "--empty" : ""}`} ref={cardStackEl}>
-      {isEmpty && (
-        <p className="CardStackEmptyMessage">
-          <Tr name="RATE_SECTION_EXHAUSTED_CARDS" />
-        </p>
-      )}
-      {Children.map(children, (child, index) => {
-        if (isValidElement(child)) {
-          if ((child.type as NamedExoticComponent)?.displayName === "Card") {
-            return cloneElement(child, {
-              ref: (cardEl: HTMLDivElement) => {
-                cardEls.current.set(index, cardEl);
+          if (item) {
+            dispatch({
+              type: "remove",
+              payload: {
+                id: item.id,
+                direction: e.code === "ArrowLeft" ? "left" : "right",
               },
             });
+            dispatch({ type: "add" });
           }
-          return child;
         }
-        return null;
-      })}
-    </div>
-  );
-};
+      };
+      window.addEventListener("keydown", handler);
+      return () => window.removeEventListener("keydown", handler);
+    }, [items]);
+
+    const bind = useGesture(
+      {
+        onDrag({ args: [id], down, delta: [x], movement: [mx] }) {
+          if (down) {
+            const p = abs(mx / (vw + ew));
+            let isAfterId = false;
+            items.forEach(({ removed, index, id: _id, controller }) => {
+              if (!removed) {
+                if (id === _id) {
+                  controller.set({
+                    x: (controller.springs as any).x.get() + x,
+                    opacity: 1 - p,
+                  });
+                  isAfterId = true;
+                } else {
+                  const yz =
+                    p * (isAfterId ? -YZ_SIZE : YZ_SIZE) +
+                    index * YZ_SIZE +
+                    YZ_OFFSET;
+                  controller.set({ y: yz, z: yz });
+                }
+              }
+            });
+          }
+        },
+        onDragEnd({ args: [id], movement: [x], velocities: [vx] }) {
+          const p = abs(x / (vw + ew));
+          if (p > 0.5 || abs(vx) > 0.5) {
+            dispatch({
+              type: "remove",
+              payload: {
+                id,
+                direction: sign(x) === -1 ? "left" : "right",
+              },
+            });
+            dispatch({ type: "add" });
+          } else {
+            items.forEach(({ removed, index, id: _id, controller }) => {
+              if (!removed) {
+                if (id === _id) {
+                  controller.start({ x: 0, opacity: 1 });
+                } else {
+                  const yz = index * YZ_SIZE + YZ_OFFSET;
+                  controller.start({ y: yz, z: yz });
+                }
+              }
+            });
+          }
+        },
+      },
+      { drag: { axis: "x" } },
+    );
+
+    const isEmpty = items.length === 0;
+
+    return (
+      <>
+        <div className={`CardStack ${isEmpty ? "--empty" : ""}`} ref={elRef}>
+          {isEmpty && (
+            <p className="CardStackEmptyText">
+              <Tr name="RATE_SECTION_EXHAUSTED_CARDS" />
+            </p>
+          )}
+          {items.map((item) => {
+            const { x, y, z, opacity } = item.controller.springs as any;
+            return (
+              <animated.div
+                key={item.id}
+                data-index={item.index}
+                className="CardStackItem"
+                style={{
+                  x,
+                  y,
+                  z,
+                  opacity: opacity.to([0.1, 0.8, 1], [0, 1, 1]),
+                }}
+                {...bind(item.id)}
+              >
+                {renderCard(item.item)}
+              </animated.div>
+            );
+          })}
+        </div>
+      </>
+    );
+  },
+  () => true,
+);
