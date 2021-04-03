4a8a7b062ec77cf94d23bbb6c61aec9601f9ccc7
Luke Channings <luke@channings.me>
2021-04-03 09:32:20 +0100

    Set the base URL and API URL in development mode. I tried to proxy /api/ws in Snowpack, but it just won't work.

--- web/app/snowpack.config.js
+++ web/app/snowpack.config.js
@@ -1,8 +1,15 @@
 const { VERSION = "dev" } = process.env;
 
+console.log(`Building version ${VERSION}`);
+
+process.env.SNOWPACK_PUBLIC_BASE_PATH = "http://localhost:8000";
+
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
-  env: { VERSION },
+  env: {
+    VERSION,
+    API_URI: VERSION === "dev" ? "ws://localhost:8000/api/ws" : undefined,
+  },
   mount: {
     static: { url: "/", static: true },
     src: { url: "/dist" },
@@ -13,7 +20,7 @@ module.exports = {
     "@snowpack/plugin-typescript",
   ],
   optimize: {
-    bundle: true,
+    bundle: VERSION !== "dev",
     minify: true,
     treeshake: true,
     target: "es2018",
