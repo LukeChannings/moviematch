7ae353e96e0bab86f4dc7c714ccbf8afd553c947
Luke Channings <luke@channings.me>
2021-04-19 10:06:25 +0100

    Fix #74

--- web/app/snowpack.config.js
+++ web/app/snowpack.config.js
@@ -3,19 +3,23 @@ const { VERSION = "dev" } = process.env;
 
 console.log(`Building version ${VERSION}`);
 
-const ip =
-  Object.values(os.networkInterfaces()).flat().find((_) =>
-    _.family == "IPv4" && !_.internal
-  ).address;
+const ip = Object.values(os.networkInterfaces())
+  .flat()
+  .find(
+    (_) => _.family == "IPv4" && !_.internal && _.address.startsWith("192"),
+  ) ?? {};
 
-process.env.SNOWPACK_PUBLIC_ROOT_PATH = `http://${ip}:8000`;
+process.env.SNOWPACK_PUBLIC_ROOT_PATH = `http://${ip.address ??
+  "localhost"}:8000`;
 
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mode: VERSION === "dev" ? "development" : "production",
   env: {
     VERSION,
-    API_URI: VERSION === "dev" ? `ws://${ip}:8000/api/ws` : undefined,
+    API_URI: VERSION === "dev"
+      ? `ws://${ip.address ?? "localhost"}:8000/api/ws`
+      : undefined,
   },
   mount: {
     static: { url: "/", static: true },
