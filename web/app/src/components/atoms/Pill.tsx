6aa4c4854a7dd900f30d76f99894c77b8aaed578
Luke Channings <luke@channings.me>
2021-05-01 23:59:07 +0100

    Add an 'open in plex' link to the card stack cards

--- web/app/src/components/atoms/Pill.tsx
+++ web/app/src/components/atoms/Pill.tsx
@@ -1,18 +1,28 @@
-import React, { ReactNode } from "react";
+import React, { HTMLAttributes, ReactNode } from "react";
 
 import styles from "./Pill.module.css";
 
 interface PillProps {
   children: ReactNode;
   onRemove?: React.MouseEventHandler<HTMLDivElement>;
+  href?: string;
 }
 
-export const Pill = ({ children, onRemove }: PillProps) => (
-  <div
-    className={onRemove ? styles.pillWithRemoveButton : styles.pill}
-    onClick={onRemove}
-  >
-    <span className={styles.label}>{children}</span>
-    {onRemove && <span className={styles.icon}>&times;</span>}
-  </div>
-);
+export const Pill = ({ children, onRemove, href }: PillProps) => {
+  const Tag = href ? "a" : "div";
+
+  const props =
+    (Tag === "a"
+      ? { href }
+      : { onClick: onRemove } as HTMLAttributes<HTMLElement>);
+
+  return (
+    <Tag
+      className={onRemove && !href ? styles.pillWithRemoveButton : styles.pill}
+      {...props}
+    >
+      <span className={styles.label}>{children}</span>
+      {onRemove && <span className={styles.icon}>&times;</span>}
+    </Tag>
+  );
+};
