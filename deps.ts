1f40499240ed1bebcc5951462002da613da49512
Luke Channings <luke@channings.me>
2021-05-31 17:44:41 +0100

    Update stdlib to 0.97.0

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 // Type re-exports
-export type { Response } from "https://deno.land/std@0.96.0/http/server.ts";
-export type { WebSocket } from "https://deno.land/std@0.96.0/ws/mod.ts";
-export type { Deferred } from "https://deno.land/std@0.96.0/async/deferred.ts";
+export type { Response } from "https://deno.land/std@0.97.0/http/server.ts";
+export type { WebSocket } from "https://deno.land/std@0.97.0/ws/mod.ts";
+export type { Deferred } from "https://deno.land/std@0.97.0/async/deferred.ts";
 
 // Deno std depenencies
 export {
@@ -9,34 +9,34 @@ export {
   Server,
   ServerRequest,
   serveTLS,
-} from "https://deno.land/std@0.96.0/http/server.ts";
-export * as log from "https://deno.land/std@0.96.0/log/mod.ts";
-export { ConsoleHandler } from "https://deno.land/std@0.96.0/log/handlers.ts";
-export { LogRecord } from "https://deno.land/std@0.96.0/log/logger.ts";
+} from "https://deno.land/std@0.97.0/http/server.ts";
+export * as log from "https://deno.land/std@0.97.0/log/mod.ts";
+export { ConsoleHandler } from "https://deno.land/std@0.97.0/log/handlers.ts";
+export { LogRecord } from "https://deno.land/std@0.97.0/log/logger.ts";
 export {
   assert,
   assertEquals,
   assertNotEquals,
-} from "https://deno.land/std@0.96.0/testing/asserts.ts";
-export { acceptWebSocket } from "https://deno.land/std@0.96.0/ws/mod.ts";
+} from "https://deno.land/std@0.97.0/testing/asserts.ts";
+export { acceptWebSocket } from "https://deno.land/std@0.97.0/ws/mod.ts";
 export {
   extname,
   join as joinPath,
   resolve as resolvePath,
-} from "https://deno.land/std@0.96.0/path/posix.ts";
+} from "https://deno.land/std@0.97.0/path/posix.ts";
 export {
   parse as parseYaml,
   stringify as stringifyYaml,
-} from "https://deno.land/std@0.96.0/encoding/yaml.ts";
-export { parse as parseFlags } from "https://deno.land/std@0.96.0/flags/mod.ts";
-export { deferred } from "https://deno.land/std@0.96.0/async/deferred.ts";
-export { readerFromStreamReader } from "https://deno.land/std@0.96.0/io/streams.ts";
-export { walk } from "https://deno.land/std@0.96.0/fs/walk.ts";
+} from "https://deno.land/std@0.97.0/encoding/yaml.ts";
+export { parse as parseFlags } from "https://deno.land/std@0.97.0/flags/mod.ts";
+export { deferred } from "https://deno.land/std@0.97.0/async/deferred.ts";
+export { readerFromStreamReader } from "https://deno.land/std@0.97.0/io/streams.ts";
+export { walk } from "https://deno.land/std@0.97.0/fs/walk.ts";
 
 // Third-party dependencies
 export { Accepts } from "https://deno.land/x/accepts@2.1.0/mod.ts";
 // @deno-types="https://cdn.skypack.dev/yup@0.32.9?dts"
-export * as yup from "https://cdn.skypack.dev/pin/yup@v0.32.9-7S9NI8QRAljrd7rctNp1/mode=imports/optimized/yup.js";
+export * as yup from "https://cdn.skypack.dev/pin/yup@v0.32.9-V1IHphwO2td6lU13qiPi/mode=imports/optimized/yup.js";
 export * as base64 from "https://deno.land/x/base64@v0.2.1/mod.ts";
 export { gzip } from "https://deno.land/x/compress@v0.3.6/mod.ts";
 export { lookup as lookupMimeType } from "https://deno.land/x/media_types@v2.7.1/mod.ts";
