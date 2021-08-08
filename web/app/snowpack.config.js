a017210cca6defb0bbe1ada015fed13138412db6
Luke Channings <luke@channings.me>
2021-08-08 18:38:14 +0100

    Snowpack: Correctly handle the client disconnecting from the websocket

--- web/app/snowpack.config.js
+++ web/app/snowpack.config.js
@@ -17,14 +17,24 @@ module.exports = {
     {
       src: "/api/ws",
       upgrade: (req, socket, head) => {
+        const defaultWSHandler = (err, _req, socket, _head) => {
+          if (err) {
+            if (err.code !== "ECONNRESET") {
+              console.error('proxy error', err);
+            }
+            socket.destroy();
+          }
+        };
+
         proxy.ws(
           req,
           socket,
           head,
           {
-            hostname: "localhost",
+            hostname: 'localhost',
             port: 8000,
           },
+          defaultWSHandler,
         );
       },
     },
